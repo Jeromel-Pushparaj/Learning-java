133ed38e1920be644c219224623272fd425ca59f
Jeromel Pushparaj <116452699+Jeormel@users.noreply.github.com>
2023-10-12 06:15:46 +0530

    first code in java

---
+++ Hello.java
@@ -0,0 +1,13 @@
+class java
+{
+    public static void main(String a[])
+    {
+        int n = 18;
+        int by = 2005;
+        int result = n + by;
+        System.out.println("Hello world");
+        System.out.println(3 + 4);
+        System.out.println(2005+n);
+        System.out.println(result);
+    }
+}
